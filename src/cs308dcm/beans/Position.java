06a9fb66505e0b11a6bd51a0d84f9e6f9b8eed98
TarikHusovic <tarikhhusovic@gmail.com>
2018-03-15 22:56:29 +0100

    Models for Staff, User, Position and Procedure entities

---
+++ src/cs308dcm/beans/Position.java
@@ -0,0 +1,21 @@
+package cs308dcm.beans;
+
+public class Position {
+	private int id;
+	private String postion;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPostion() {
+		return postion;
+	}
+	public void setPostion(String postion) {
+		this.postion = postion;
+	}	
+}
+
---
+++ src/cs308dcm/beans/Procedure.java
@@ -0,0 +1,22 @@
+package cs308dcm.beans;
+
+public class Procedure {
+	private int id;
+	private String procedure;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProcedure() {
+		return procedure;
+	}
+	public void setProcedure(String procedure) {
+		this.procedure = procedure;
+	}
+	
+	
+}
